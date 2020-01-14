8a097edc622f95009294d798579e316b94607994
unknown <Administrador@CBS.COM>
2020-01-14 16:38:07 +0100

    Front End

--- index.php
+++ index.php
@@ -1,30 +1,55 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 <html>
-    
-    <script>
-        function loadDoc() {
-            var xhttp = new XMLHttpRequest();
+	<head>
+		<title>Miniport by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
 
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("demo2").innerHTML =
-                this.responseText;
-                }
-            };
+		<!-- Nav -->
+			<!--nav id="nav">
+				<ul class="container">
+					<li><a href="#top">Random</a></li>
+					<li><a href="#work">Add Word</a></li>
+				</ul>
+			</nav-->
 
-            xhttp.open("GET", "randomWord.php", true);
-            xhttp.send();
-        }
-    </script>
-
-    <body>
-        <div id="demo">
-            <h1>The XMLHttpRequest Object</h1>
-            <button type="button" onclick="loadDoc()">Random Word</button>
-            
-            <div id="demo2">
-
-            </div>
-        </div>
-    </body>
+		<!-- Home -->
+			<div class="container">
+				<div class="logo">
+					<p><img src="./images/logo-bonsoleil.png" /></p>
+				</div>
+				<div class="words">
+					<h2>Honorificabilitudinitatibus</h2>
+					<p><button href="#" class="btn_word" onclick="">Word</button></p>
+				</div>
+				<div class="filters level">
+					<h3>LEVEL</h3>
+					<ul>
+						<li><input type="checkbox" id="pet" name="pet" /><label for="pet"><span></span>PET</label></li>
+						<li><input type="checkbox" id="ket" name="ket" /><label for="ket"><span></span>KET</label></li>
+						<li><input type="checkbox" id="first" name="first" /><label for="first"><span></span>FIRST</label></li>
+						<li><input type="checkbox" id="advance" name="advance" /><label for="advance"><span></span>ADVANCE</label></li>
+					</ul>
+					<h3>FILTERS</h3>
+					<ul>
+						<li><input type="checkbox" id="pet" name="pet" /><label for="pet"><span></span>PET</label></li>
+						<li><input type="checkbox" id="ket" name="ket" /><label for="ket"><span></span>KET</label></li>
+						<li><input type="checkbox" id="first" name="first" /><label for="first"><span></span>FIRST</label></li>
+						<li><input type="checkbox" id="advance" name="advance" /><label for="advance"><span></span>ADVANCE</label></li>
+					</ul>
+				</div>
+				<div class="add">
+					<a href="" class="plus"><span class="none">plus</span></a>
+				</div>
+			</div>
+		<!-- Scripts -->
+	</body>
 </html>
